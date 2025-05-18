810187d375a492ceab0f65aebd548bdae0fb0e80
tomek_jablko <trunio31@gmail.com>
2025-05-18 14:55:52 +0900

    Deleted double imports

--- logic/RandomPlayer.py
+++ logic/RandomPlayer.py
@@ -1,12 +1,10 @@
 import numpy as np
-
 from logic.Player import Player
-import numpy
 
 class RandomPlayer(Player):
     def ask_decision(self, available_decisions):
         action = np.random.choice(available_decisions)
         if action == 'raise':
-            return 'raise ' + str(numpy.random.randint(1, int(self.chips) / 10  + 1))
+            return 'raise ' + str(np.random.randint(1, int(self.chips) / 10  + 1))
         return action
 
